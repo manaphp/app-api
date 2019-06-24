05eb5275f835c5c9b26a4a056270a6599de680dc
ManaPHP Mark <manaphp@qq.com>
2019-06-24 17:34:57 +0800

    Modified   config/app.php

--- config/app.php
+++ config/app.php
@@ -7,16 +7,12 @@ return [
     'version' => '1.1.1',
     'timezone' => 'PRC',
     'master_key' => env('MASTER_KEY'),
-    'compatible_globals' => false,
-    'servers' => [
-        'http' => ['worker_num' => 1, 'max_request' => 1000000, 'dispatch_mode' => 1]
-    ],
     'params' => [],
     'aliases' => [],
     'components' => [
+        '!httpServer' => ['port' => 9501, 'worker_num' => 1, 'max_request' => 1000000, 'dispatch_mode' => 1],
         'db' => [env('DB_URL')],
         'redis' => [env('REDIS_URL')],
-        'mongodb' => [env('MONGODB_URL')],
         'logger' => ['level' => env('LOGGER_LEVEL', 'info')],
     ],
     'services' => [],

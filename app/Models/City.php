cf820297422de82c568642c52ed0c4909823df90
ManaPHP Mark <manaphp@qq.com>
2019-11-17 10:02:47 +0800

    Modified   app/Models/City.php Modified   app/Models/Country.php

--- app/Models/City.php
+++ app/Models/City.php
@@ -1,14 +1,32 @@
 <?php
 namespace App\Models;
 
+use ManaPHP\Db\Model;
+
 /**
- * Class City
+ * Class App\Models\City
  * @property \App\Models\Country $country
  */
-class City extends \ManaPHP\Db\Model
+class City extends Model
 {
     public $city_id;
     public $city;
     public $country_id;
     public $last_update;
+
+    /**
+     * @return string
+     */
+    public function getTable()
+    {
+        return 'city';
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrimaryKey()
+    {
+        return 'city_id';
+    }
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -1,13 +1,31 @@
 <?php
 namespace App\Models;
 
+use ManaPHP\Db\Model;
+
 /**
- * Class Country
+ * Class App\Models\Country
  * @property-read \App\Models\City $cities
  */
-class Country extends \ManaPHP\Db\Model
+class Country extends Model
 {
     public $country_id;
     public $country;
     public $last_update;
+
+    /**
+     * @return string
+     */
+    public function getTable()
+    {
+        return 'country';
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrimaryKey()
+    {
+        return 'country_id';
+    }
 }

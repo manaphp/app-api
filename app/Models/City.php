6e49005606a67214d201bf99c7fa23ba22584eaf
ManaPHP Mark <manaphp@qq.com>
2020-06-24 10:15:54 +0800

    Modified   app/Models/City.php Modified   app/Models/Country.php Modified   app/Router.php Modified   docker/docker-compose.yml Modified   docker/swoole.yml

--- app/Models/City.php
+++ app/Models/City.php
@@ -1,32 +1,16 @@
 <?php
+
 namespace App\Models;
 
-use ManaPHP\Db\Model;
-
 /**
- * Class App\Models\City
+ * Class City
+ *
  * @property \App\Models\Country $country
  */
-class City extends Model
+class City extends \ManaPHP\Db\Model
 {
     public $city_id;
     public $city;
     public $country_id;
     public $last_update;
-
-    /**
-     * @return string
-     */
-    public function getTable()
-    {
-        return 'city';
-    }
-
-    /**
-     * @return string
-     */
-    public function getPrimaryKey()
-    {
-        return 'city_id';
-    }
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -1,31 +1,15 @@
 <?php
+
 namespace App\Models;
 
-use ManaPHP\Db\Model;
-
 /**
- * Class App\Models\Country
+ * Class Country
+ *
  * @property-read \App\Models\City $cities
  */
-class Country extends Model
+class Country extends \ManaPHP\Db\Model
 {
     public $country_id;
     public $country;
     public $last_update;
-
-    /**
-     * @return string
-     */
-    public function getTable()
-    {
-        return 'country';
-    }
-
-    /**
-     * @return string
-     */
-    public function getPrimaryKey()
-    {
-        return 'country_id';
-    }
 }

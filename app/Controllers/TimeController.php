bf6a776289828a072465c6339e9ee920d7021dcc
ManaPHP Mark <manaphp@qq.com>
2020-11-20 10:24:53 +0800

    Modified   app/Commands/TestCommand.php Modified   app/Controllers/TestController.php Modified   app/Controllers/TimeController.php Missing    app/Models/.gitkeep Modified   app/Models/City.php Modified   app/Models/Country.php Missing    app/Servcies/.gitkeep Added      app/Commands/Command.php Added      app/Controllers/Controller.php Added      app/Models/Model.php Added      app/Services/Service.php

--- app/Controllers/TimeController.php
+++ app/Controllers/TimeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controllers;
 
-use ManaPHP\Rest\Controller;
-
 class TimeController extends Controller
 {
     public function getAcl()
     {
         return ['*' => '*'];
+    }
+
+    public function helloAction()
+    {
+        return $this->response->setContent('hello world!');
     }
 
     public function currentAction()
